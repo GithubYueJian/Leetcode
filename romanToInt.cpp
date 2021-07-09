d4132b03ecfa71f4e7f67e930addee7f4d8b1747
GithubYueJian <56901351+GithubYueJian@users.noreply.github.com>
2021-07-09 10:47:25 +0800

    Create romanToInt.cpp

---
+++ romanToInt.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    unordered_map<char,int> roman_map=
+    {
+        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000},
+    };
+public:
+    int romanToInt(string s) 
+    {
+        int y =0;
+        for(int i=0;i<s.size();++i)
+        {
+           if((i!=(s.size()-1))&&(roman_map[s[i]]<roman_map[s[i+1]]))
+           {
+               y=y-roman_map[s[i]];
+           }
+           else
+           {
+               y=y+roman_map[s[i]];
+           }
+          
+
+ 
+        }
+        return y;
+
+    }
+};
