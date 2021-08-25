f264bbbb10de2552c29dc22295161af15db71249
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-08-25 13:54:24 +0800

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    /*
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2);
+        for(int i=0;i<nums.size();++i)
+        {
+            for(int j=i+1;j<=nums.size()-1;++j)
+            {
+                if((nums[i]+nums[j])==target)
+                {
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+               
+            }
+        }
+    return ans;
+    }
+    */
+    
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> hash_map;
         for(int index_i = 0;index_i<nums.size();++index_i)
