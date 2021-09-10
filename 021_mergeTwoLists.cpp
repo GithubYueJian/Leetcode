501ae3fd1d49b21668113384301883c6fcc269c2
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-09-10 22:48:50 +0800

    Update 021_mergeTwoLists.cpp

--- 021_mergeTwoLists.cpp
+++ 021_mergeTwoLists.cpp
@@ -10,29 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode head = ListNode(0);
-        ListNode* p = &head;
-        while(l1!=nullptr&&l2!=nullptr)
-        {
-            if(l1->val < l2->val)
-            {
-                p->next=l1;
-                l1=l1->next;
-                p=p->next;
-            }
-            else
-            {
-                p->next=l2;
-                l2=l2->next;
-                p=p->next;
-            }
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+	ListNode* head = new ListNode(0);
+	ListNode* p = head;
+	while (l1 != nullptr&&l2 != nullptr)
+	{
+		if (l1->val < l2->val)
+		{
+			p->next = l1;
+			l1 = l1->next;
+			p = p->next;
+		}
+		else
+		{
+			p->next = l2;
+			l2 = l2->next;
+			p = p->next;
+		}
 
-        }
-        p->next = l1 == nullptr ? l2 : l1;
+	}
+	p->next = l1 == nullptr ? l2 : l1;
 
-        return head.next;
-
-
-    }
+	return head->next;
+}
 };
