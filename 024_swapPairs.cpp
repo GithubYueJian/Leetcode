d6a63515ae70fc815c3248d54efd4db16b3930e0
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-09-11 20:13:23 +0800

    Create 024_swapPairs.cpp

---
+++ 024_swapPairs.cpp
@@ -0,0 +1,11 @@
+ListNode* swapPairs(ListNode* head)
+{
+	if (head == nullptr || head->next == nullptr) {
+		return head;
+	}
+
+	ListNode* newHead = head->next;
+	head->next = swapPairs(newHead->next);
+	newHead->next = head;
+	return newHead;
+}
