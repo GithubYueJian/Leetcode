497da485e6075506b70abb829b1a2bdf7f7b7e8c
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-09-12 16:05:20 +0800

    Create 026_removeDuplicates.cpp

---
+++ 026_removeDuplicates.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    vector<int>temp;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (i > 0 && nums[i] == nums[i - 1])
+			continue;
+		temp.push_back(nums[i]);
+	}
+	nums = temp;
+	return nums.size();
+
+    }
+};
