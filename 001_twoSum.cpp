fcde033beea051ac8a92f92dc9dfef1411690ad6
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2022-04-04 12:11:29 +0800

    去掉暴力解法，只有哈希表解法

--- 001_twoSum.cpp
+++ 001_twoSum.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    /*
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans(2);
-        for(int i=0;i<nums.size();++i)
-        {
-            for(int j=i+1;j<=nums.size()-1;++j)
-            {
-                if((nums[i]+nums[j])==target)
-                {
-                    ans[0]=i;
-                    ans[1]=j;
-                }
-               
-            }
+        unordered_map<int,int> hashmap;
+        for(int i=0;i<nums.size();++i) {
+           auto it = hashmap.find(target-nums[i]);
+           if(it!=hashmap.end())
+               return {it->second,i};
+           else
+           hashmap[nums[i]]=i;
         }
-    return ans;
-    }
-    */
-    
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> hash_map;
-        for(int index_i = 0;index_i<nums.size();++index_i)
-        {
-            auto it = hash_map.find(target-nums[index_i]);
-            if(it!=hash_map.end())
-            {
-                return {it->second,index_i};
-            }
-            hash_map[nums[index_i]]=index_i;
-        }
-
         return {};
     }
 };
+
+
