f0b46962db6090fd629f3a4a208d4e540685b5bd
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2022-04-10 13:13:18 +0800

    Update 020_isValid.cpp

--- 020_isValid.cpp
+++ 020_isValid.cpp
@@ -1,37 +1,68 @@
-class Solution {
-    unordered_map<char,char> pairs={
-        {'(',')'},
-        {'[',']'},
-        {'{','}'},
-    };
-    
-public:
-    bool isValid(string s) 
-    {
-        stack<char> operators;
-        if((s.size()%2==1)||(s.size()==0))
-            return false;
-        else
-        {
-            
-            for(int i=0;i<s.size();++i)
-            {
-                if(operators.empty())
-                     operators.push(s[i]);
-                else
-                {
-                if(pairs[operators.top()]!=s[i])
-                    operators.push(s[i]);
-                else
-                    operators.pop();
-                }
-                
-            }
-        }
-        if(operators.empty())
-            return true;
-        else
-            return false;
+/*
+* Purpose: isValid,LeetCode 20
+* Author: Jian Yue
+* Data:   2021/09/09,revised 2022/04/10
+* Refrence: ***
+*/
 
-    }
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+unordered_map<char, char> umap = {
+    {'(',')'},
+    {'[',']'},
+    {'{','}'},
 };
+bool isValid(string s)
+{
+    stack<char> stk;
+    //如果字符串长度为0，返回true
+    if (s.size() == 0)
+        return true;
+    //如果字符串长度是奇数，返回false
+    if (s.size() % 2 == 1) 
+        return false;
+    else
+    {
+
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (stk.empty())
+                stk.push(s[i]);
+            else
+            {
+                //判断当前字符与栈顶元素是否匹配
+                if (s[i] == umap[stk.top()])
+                    stk.pop();
+                else
+                    stk.push(s[i]);
+            }
+
+        }
+    }
+    // 最后栈中没有元素，表示完全匹配
+    return stk.empty();
+
+}
+
+
+
+int main()
+{
+    bool test = isValid("()");
+    test = isValid("()[]{}");
+    test = isValid("(]");
+    test = isValid("([)]");
+    test = isValid("{[]}");
+    test = isValid(")(]");
+    test = isValid("((((]");
+
+    
+    return 0;
+}
